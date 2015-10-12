6851644bb6560b1cf637562672392baa8ec60755
Ben B <bburdette@gmail.com>
2015-10-11 22:43:59 -0600

    add files

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+extern crate websocket;
+
+use std::thread;
+use websocket::{Server, Message, Sender, Receiver};
+use websocket::header::WebSocketProtocol;
+
+use std::string::*;
+
+fn main() {
+
+	let server = Server::bind("127.0.0.1:1234").unwrap();
+
+	for connection in server {
+		// Spawn a new thread for each connection.
+		thread::spawn(move || {
+			let request = connection.unwrap().read_request().unwrap(); // Get the request
+			let headers = request.headers.clone(); // Keep the headers so we can check them
+			
+			request.validate().unwrap(); // Validate the request
+			
+			let mut response = request.accept(); // Form a response
+			
+			if let Some(&WebSocketProtocol(ref protocols)) = headers.get() {
+				if protocols.contains(&("rust-websocket".to_string())) {
+					// We have a protocol we want to use
+					response.headers.set(WebSocketProtocol(vec!["rust-websocket".to_string()]));
+				}
+			}
+			
+			let mut client = response.send().unwrap(); // Send the response
+			
+			let ip = client.get_mut_sender()
+				.get_mut()
+				.peer_addr()
+				.unwrap();
+			
+			println!("Connection from {}", ip);
+			
+			let message = Message::Text("Hello".to_string());
+			client.send_message(message).unwrap();
+			
+			let (mut sender, mut receiver) = client.split();
+			
+			for message in receiver.incoming_messages() {
+				let message = message.unwrap();
+        println!("message: {:?}", message);
+			
+        let replyy = Message::Text("replyyyblah".to_string());
+	
+				match message {
+					Message::Close(_) => {
+						let message = Message::Close(None);
+						sender.send_message(message).unwrap();
+						println!("Client {} disconnected", ip);
+						return;
+					}
+					Message::Ping(data) => {
+            println!("Message::Ping(data)");
+						let message = Message::Pong(data);
+						sender.send_message(message).unwrap();
+					}
+					// _ => sender.send_message(message).unwrap(),
+					_ => { 
+            println!("sending replyy");
+            sender.send_message(replyy).unwrap()
+            }
+				}
+			}
+		});
+	}
+}
+
